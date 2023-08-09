a0cce8ef7381e1e81ca5ddc08e7c0e5b3dd4519c
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-08-10 00:51:59 +0300

    Set the logic for loadMain function

--- src/loader.js
+++ src/loader.js
@@ -2,31 +2,74 @@
 const loadHeader = () => {
 
     const header = document.createElement('div'); 
-    
+    header.setAttribute('id', 'header'); 
+
+    const logoContainer = document.createElement('div'); 
+
     const logo = document.createElement('img'); 
     logo.setAttribute('id', 'logo'); 
 
+    const title = document.createElement('h1'); 
+    title.textContent = "Ouroboros Bistro";
+
+    logoContainer.append(logo, title);
+
     const nav = document.createElement('nav'); 
-    const brunch = document.createElement('a');
+    const brunch = document.createElement('button');
     brunch.textContent = "Brunch"; 
 
-    const starters = document.createElement('a');
+    const starters = document.createElement('button');
     starters.textContent = "Starters"; 
 
-    const mains = document.createElement('a'); 
+    const mains = document.createElement('button'); 
     mains.textContent = "Mains"; 
 
-    nav.append(brunch, starters, mains); 
+    const drinks = document.createElement('button'); 
+    drinks.textContent = "Drinks"; 
 
-    header.append(logo, nav); 
+    nav.append(brunch, starters, mains, drinks); 
+
+    header.append(logoContainer, nav); 
     return header; 
 
 }
 
 const loadMain = () => {
 
+    const container = document.createElement('div');
+
+    const header = document.createElement('div'); 
+
+    const mainHeader = document.createElement('h1'); 
+    mainHeader.textContent = "Ouroboros"; 
+
+    const caption = document.createElement('h4'); 
+    caption.textContent = "Extraordinary food since 2017";
+
+    header.append(mainHeader, caption);
+
+    const contact = document.createElement('div'); 
+    const instagram = document.createElement('img'); 
+    instagram.setAttribute('id', 'instagram'); 
+    
+    const facebook = document.createElement('img');
+    facebook.setAttribute('id', 'facebook');
+
+    const number = document.createElement('p'); 
+    number.textContent = "+9647712345678"; 
+    contact.append(instagram, facebook, number);
+
+    container.append(header, contact);
+
+    return container; 
 }
 
 const loadMenu = () => {
 
 }
+
+export {
+    loadHeader, 
+    loadMain, 
+    loadMenu
+};
